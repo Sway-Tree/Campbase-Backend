7f61efaa2de7a2a4a269310f293fe196d12c8937
Sway-Tree <agnieszka.dudek92@gmail.com>
2014-01-27 19:35:49 +0000

    Added trip controller

--- Gemfile
+++ Gemfile
@@ -42,6 +42,12 @@ group :doc do
   gem 'sdoc', require: false
 end
 
+gem 'pry'
+
+gem 'rack-cors', :require => 'rack/cors'
+
+
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -69,6 +69,6 @@ class TripsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def trip_params
-      params.require(:trip).permit(:name, :place, :price, :from, :to, :photo, :description)
+      params.permit(:name, :place, :price, :from, :to, :photo, :description)
     end
 end
